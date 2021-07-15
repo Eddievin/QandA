dd10d5a13d0fbb1f4604a02e7778a9b05011f420
Eddievin <thokerunga200@gmail.com>
2021-07-15 06:58:59 +0300

    Updated code

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,7 @@ Route::get('/pages/profile', 'QuestionsController@profile');
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
